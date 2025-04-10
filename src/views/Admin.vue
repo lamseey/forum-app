e28d6179710db42d35dfb736513f6723acd25594
Mouad <mouadensafir@gmail.com>
2025-04-10 23:52:40 +0100

    Router + View Configurations

---
+++ src/views/Admin.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>ADMIN PAGE</h1>
+</template>
+
+<script>
+export default {
+  name: 'admin-view'
+}
+</script>
---
+++ src/views/Discussion.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Discussion PAGE</h1>
+</template>
+
+<script>
+export default {
+  name: 'discussion-view'
+}
+</script>
