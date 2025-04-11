56c1966c8afb42147bacf3affd2bd8455f3fe618
akito-sama <ahmedquadimi22@gmail.com>
2025-04-11 19:29:32 +0100

    adding logout functionnality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,22 +3,36 @@
     <router-link to="/">Home</router-link>
     <router-link to="/register">Register</router-link>
     <router-link to="/login">Login</router-link>
+    <button @click="logout">Logout</button>
   </nav>
 </template>
 
+<script setup>
+import { auth } from "@/firebase";
+import { signOut } from "firebase/auth";
+const logout = async () => {
+  try {
+    await signOut(auth);
+    console.log("lougged out sucessfully");
+  } catch (err) {
+    console.log("there was a error while logging out");
+  }
+};
+</script>
+
 <style>
-  nav {
-    background-color: #333;
-    color: white;
-    padding: 10px;
-    text-align: center;
-  }
-  nav a {
-    color: white;
-    margin-right: 10px;
-    text-decoration: none;
-  }
-  nav a:hover {
-    text-decoration: underline;
-  }
+nav {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+nav a {
+  color: white;
+  margin-right: 10px;
+  text-decoration: none;
+}
+nav a:hover {
+  text-decoration: underline;
+}
 </style>
